714496fc6895d48b5e5b0bdeac659445a7a3ccd4
Thierry Parent <tparent@absolunet.com>
2018-07-03 09:40:19 -0400

    Allow batch call as well

--- src/gee/config/endpoints/endpoint/add-to-cart.js
+++ src/gee/config/endpoints/endpoint/add-to-cart.js
@@ -1,7 +1,10 @@
 const { url:urlHelper } = require('./../../../../helpers');
 
 module.exports = {
-	endpoint: '/carts/current/cartlines',
+	endpoint: '/carts/current/cartlines(/batch)?',
 	event: 'addToCart',
-	method: urlHelper.methods.post
+	method: urlHelper.methods.post,
+	preprocess: (response) => {
+		response.products = response.cartLines || [angular.copy(response)];
+	}
 };
