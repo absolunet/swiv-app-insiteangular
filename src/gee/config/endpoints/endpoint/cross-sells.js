1a7bdc46ef0cc898e859bb0215ae535d54fa609e
Thierry Parent <tparent@absolunet.com>
2018-07-06 10:02:55 -0400

    Set proper list name

--- src/gee/config/endpoints/endpoint/cross-sells.js
+++ src/gee/config/endpoints/endpoint/cross-sells.js
@@ -1,4 +1,7 @@
 module.exports = {
 	endpoint: '/websites/current/crosssells',
-	event: 'productImpression'
+	event: 'productImpression',
+	preprocess: (response) => {
+		response.list = 'Web cross sale';
+	}
 };
