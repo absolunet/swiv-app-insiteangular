df2527092933062ffb3653049da651bbbc72f1ec
Thierry Parent <tparent@absolunet.com>
2018-07-06 10:02:40 -0400

    Manage quantity added to cart

--- src/gee/config/endpoints/endpoint/add-to-cart.js
+++ src/gee/config/endpoints/endpoint/add-to-cart.js
@@ -4,7 +4,11 @@ module.exports = {
 	endpoint: '/carts/current/cartlines(/batch)?',
 	event: 'addToCart',
 	method: urlHelper.methods.post,
-	preprocess: (response) => {
+	preprocess: (response, request) => {
 		response.products = response.cartLines || [angular.copy(response)];
+		response.list = '';
+		response.products.forEach((product) => {
+			product.qtyAdded = request.qtyOrdered || product.qtyOrdered;
+		});
 	}
 };
