cf9ac980812d3255b28446b00b7825fb6575aed6
Thierry Parent <tparent@absolunet.com>
2018-07-06 10:17:51 -0400

    Fix usage of service deconstruction

--- src/gee/config/endpoints/endpoint/current-session.js
+++ src/gee/config/endpoints/endpoint/current-session.js
@@ -1,6 +1,6 @@
 module.exports = {
 	endpoint: '/sessions/current',
-	process: (response, request, geeService) => {
+	process: (response, request, { geeService }) => {
 		if (response && response.currency && response.currency.currencyCode) {
 			geeService.configs.set('currencyCode', response.currency.currencyCode);
 		}
